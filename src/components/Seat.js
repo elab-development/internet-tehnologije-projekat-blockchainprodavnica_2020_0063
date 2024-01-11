403748253b705336c526420601dfd9c21795ce58
Vanjola <vanjagacic127@gmail.com>
2024-01-11 17:40:01 +0100

    Ispravka gresaka

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -1,16 +1,33 @@
+// Seat.js
+
+import { useEffect, useState } from 'react';
+
 const Seat = ({ i, step, columnStart, maxColumns, rowStart, maxRows, seatsTaken, buyHandler }) => {
-    return (
-        <div
-            onClick={() => buyHandler(i + step)}
-            className={seatsTaken.find(seat => Number(seat) == i + step) ? "occasion__seats--taken" : "occasion__seats"}
-            style={{
-                gridColumn: `${((i % maxColumns) + 1) + columnStart}`,
-                gridRow: `${Math.ceil(((i + 1) / maxRows)) + rowStart}`
-            }}
-        >
-            {i + step}
-        </div>
-    );
-}
+  const isSeatTaken = seatsTaken.find(seat => Number(seat) === i + step);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    setIsDisabled(isSeatTaken);
+  }, [isSeatTaken]);
+
+  const handleSeatClick = () => {
+    if (!isDisabled) {
+      buyHandler(i + step);
+    }
+  };
+
+  return (
+    <div
+      onClick={handleSeatClick}
+      className={`occasion__seats ${isSeatTaken ? 'occasion__seats--taken' : ''}`}
+      style={{
+        gridColumn: `${((i % maxColumns) + 1) + columnStart}`,
+        gridRow: `${Math.ceil(((i + 1) / maxRows)) + rowStart}`
+      }}
+    >
+      {i + step}
+    </div>
+  );
+};
 
 export default Seat;
